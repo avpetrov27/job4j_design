1a8d8a4070cb564e494f727c236acd0ec699949e
avpetrov27 <avpetrov27@yandex.ru>
2023-04-27 13:56:01 +0300

    1. Что такое итератор. [#4951] - Урок. junit-5/assertj и junit-4/hamcrest

--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -1,27 +1,37 @@
 package ru.job4j.iterator;
 
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.is;
 
-import static org.assertj.core.api.Assertions.*;
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
 
-class ArrayItTest {
+import java.util.NoSuchElementException;
+
+public class ArrayItTest {
     @Test
-    void whenMultiCallHasNextThenTrue() {
+    public void whenMultiCallhasNextThenTrue() {
         ArrayIt it = new ArrayIt(
                 new int[]{1, 2, 3}
         );
-        boolean rsl = it.hasNext();
-        assertThat(rsl).isTrue();
-        assertThat(it.hasNext()).isTrue();
+        MatcherAssert.assertThat(it.hasNext(), is(true));
+        MatcherAssert.assertThat(it.hasNext(), is(true));
     }
 
     @Test
-    void whenReadSequence() {
+    public void whenReadSequence() {
         ArrayIt it = new ArrayIt(
                 new int[]{1, 2, 3}
         );
-        assertThat(it.next()).isEqualTo(1);
-        assertThat(it.next()).isEqualTo(2);
-        assertThat(it.next()).isEqualTo(3);
+        MatcherAssert.assertThat(it.next(), is(1));
+        MatcherAssert.assertThat(it.next(), is(2));
+        MatcherAssert.assertThat(it.next(), is(3));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextFromEmpty() {
+        ArrayIt it = new ArrayIt(
+                new int[]{}
+        );
+        it.next();
     }
 }
