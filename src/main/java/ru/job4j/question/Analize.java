875aae3986687d999dae1870932b5f0e81ce7410
avpetrov27 <avpetrov27@yandex.ru>
2023-05-11 15:24:57 +0300

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,45 @@
+package ru.job4j.question;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Analize {
+    /**
+     * diff - Метод определяет разницу между двумя множествами previous м current.
+     * Возвращается объект Info, хранящий три параметра типа int:
+     * added - количество добавленных элементов(элемент есть в current, но нет в previous);
+     * changed - количество изменённых элементов(элемент есть в обоих множествах, при этом:
+     * User.id совпадает, User.name не совпадает);
+     * deleted - количество удалённых элементов(элемента нет в current, но есть в previous).
+     * Смотри {@link User}, {@link Info}
+     * <p>
+     * Метод состоит из двух циклов
+     * 1. В первом проверяется вхождение элементов previous в current.
+     * 2. Во втором проверяется вхождение элементов current в previous.
+     *
+     * @param previous - входящее множество("старое" состояние)
+     * @param current  - входящее множество("текущее" состояние)
+     * @return - объект {@link Info}
+     */
+    public static Info diff(Set<User> previous, Set<User> current) {
+        int added = 0, changed = 0, deleted = 0;
+        HashMap<Integer, String> mapEqKey = new HashMap<>();
+        for (User userPrev : previous) {
+            if (current.contains(userPrev)) {
+                mapEqKey.put(userPrev.hashCode(), userPrev.getName());
+            } else {
+                deleted++;
+            }
+        }
+        for (User userCur : current) {
+            if (previous.contains(userCur)) {
+                if (!mapEqKey.get(userCur.hashCode()).equals(userCur.getName())) {
+                    changed++;
+                }
+            } else {
+                added++;
+            }
+        }
+        return new Info(added, changed, deleted);
+    }
+}
---
+++ src/main/java/ru/job4j/question/User.java
@@ -0,0 +1,32 @@
+package ru.job4j.question;
+
+public class User {
+    private final int id;
+    private final String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
