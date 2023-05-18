0bf0218c12f230fed7a116426a109d0c8f2a176c
avpetrov27 <avpetrov27@yandex.ru>
2023-05-18 15:15:58 +0300

    7. Анализ доступности сервера [#859 #416963]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -8,27 +8,17 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(target)))) {
-            read.lines()
-                    .map(String::trim)
-                    .map(s -> s.split(" ", 2))
-                    .filter(s -> filterForPrint(s[0]))
-                    .forEach(s -> printUnavailable(s[1], out));
+            String line;
+            while ((line = read.readLine()) != null) {
+                String[] s = line.trim().split(" ", 2);
+                if (("400".equals(s[0]) || "500".equals(s[0])) ^ unavailableFlag) {
+                    out.write(s[1] + ";" + (unavailableFlag ? System.lineSeparator() : ""));
+                    unavailableFlag = !unavailableFlag;
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private boolean filterForPrint(String s0) {
-        return (!unavailableFlag && ("400".equals(s0) || "500".equals(s0)))
-                || (unavailableFlag && ("200".equals(s0) || "300".equals(s0)));
-    }
-
-    private void printUnavailable(String s1, PrintWriter out) {
-        out.write(s1 + ";");
-        if (unavailableFlag) {
-            out.write(System.lineSeparator());
-        }
-        unavailableFlag = !unavailableFlag;
     }
 
     public static void main(String[] args) {
