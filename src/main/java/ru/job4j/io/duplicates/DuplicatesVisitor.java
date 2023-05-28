7e173e27abd414b4dc69254be11cb7654dbccad4
avpetrov27 <avpetrov27@yandex.ru>
2023-05-28 14:38:17 +0300

    11. Поиск дубликатов [#315066 #419121]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,36 +8,24 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final Map<Path, FileProperty> map = new TreeMap<>(
-            Comparator.comparing(Path::getFileName)
-                    .thenComparing(p -> p.toFile().length())
-                    .thenComparing(p -> p)
-    );
+    private final Map<FileProperty, List<Path>> map = new LinkedHashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        map.put(file, new FileProperty(file.toFile().length(), file.getFileName().toString()));
+        FileProperty newKey = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        map.putIfAbsent(newKey, new LinkedList<>());
+        map.get(newKey).add(file);
         return super.visitFile(file, attrs);
     }
 
     public void getDuplicate() {
-        Iterator<Map.Entry<Path, FileProperty>> iteratorMap = map.entrySet().iterator();
-        Map.Entry<Path, FileProperty> prev = null;
-        boolean flagDuplicate = false;
-        while (iteratorMap.hasNext()) {
-            Map.Entry<Path, FileProperty> next = iteratorMap.next();
-            if (prev != null && next.getValue().equals(prev.getValue())) {
-                if (!flagDuplicate) {
-                    System.out.printf("%s - %d%n", prev.getValue().getName(),
-                            prev.getValue().getSize());
-                    System.out.printf("\t%s%n", prev.getKey());
-                    flagDuplicate = true;
-                }
-                System.out.printf("\t%s%n", next.getKey());
-            } else {
-                flagDuplicate = false;
-            }
-            prev = next;
-        }
+        map.entrySet()
+                .stream()
+                .filter(k -> k.getValue().size() > 1)
+                .forEach(k -> {
+                    FileProperty key = k.getKey();
+                    System.out.println(key.getName() + " - " + key.getSize());
+                    k.getValue().forEach(v -> System.out.println("\t" + v));
+                });
     }
 }
