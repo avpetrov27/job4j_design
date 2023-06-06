02fbeff69908898dbf475e92d9f7231cb31f62a1
avpetrov27 <avpetrov27@yandex.ru>
2023-06-06 18:32:54 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,8 +3,12 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
+    private final static String EXIT = "Exit";
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -15,8 +19,14 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String firstLine = in.readLine();
                     System.out.println(firstLine);
-                    if (firstLine.contains("?msg=Bye")) {
-                        server.close();
+                    Matcher matcher = Pattern.compile("\\?msg=\\S+").matcher(firstLine);
+                    if (matcher.find()) {
+                        String msg = matcher.group().split("=", 2)[1];
+                        if (EXIT.equals(msg)) {
+                            server.close();
+                        } else {
+                            out.write(msg.getBytes());
+                        }
                     }
                     for (String str = in.readLine(); str != null && !str.isEmpty();
                          str = in.readLine()) {
