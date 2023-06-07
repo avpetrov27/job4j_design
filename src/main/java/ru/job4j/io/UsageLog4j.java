3afede90f5a144d687705f25000e331bd386063a
avpetrov27 <avpetrov27@yandex.ru>
2023-06-07 17:46:18 +0300

    4. Slf4j - вывод exception. [#268853] - Урок. Пример вывода Exception в лог.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,22 +7,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        boolean a = true;
-        byte b = 50;
-        char c = 0x266B;
-        short d = 1000;
-        int e = 123_456_789;
-        float f = 1.0f;
-        long g = 0xffffffffL;
-        double h = 340.01e16D;
-        LOG.debug("User info a : {},"
-                        + " b : {},"
-                        + " c : {},"
-                        + " d : {},"
-                        + " e : {},"
-                        + " f : {},"
-                        + " g : {},"
-                        + " h : {}",
-                a, b, c, d, e, f, g, h);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
