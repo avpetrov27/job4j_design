b64e6aa51e981c115cd35cbdbaae3a4d65492538
avpetrov27 <avpetrov27@yandex.ru>
2023-06-09 00:51:16 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/Main.java
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -2,15 +2,32 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
         final Person person = new Person(false, 30, "Moscow", new Contact("11-111"),
-                new String[]{"Worker", "Married"});
+                "Worker", "Married");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.getSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("address", person.getAddress());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+        System.out.println(jsonObject);
+        JSONObject jsonPerson = new JSONObject(person);
+        System.out.println(jsonPerson);
         final Gson gson = new GsonBuilder().create();
-        final String personJson = gson.toJson(person);
-        System.out.println(personJson);
-        final Person personBack = gson.fromJson(personJson, Person.class);
+        final Person personBack = gson.fromJson(String.valueOf(jsonPerson), Person.class);
         System.out.println(personBack);
     }
 }
--- src/main/java/ru/job4j/serialization/json/Person.java
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -2,19 +2,39 @@ package ru.job4j.serialization.json;
 
 import java.util.Arrays;
 
-public class Person {
+public class Person implements java.io.Serializable {
     private final boolean sex;
     private final int age;
     private final String address;
     private final Contact contact;
     private final String[] statuses;
 
-    public Person(boolean sex, int age, String address, Contact contact, String[] statuses) {
+    public Person(boolean sex, int age, String address, Contact contact, String... statuses) {
         this.sex = sex;
         this.age = age;
         this.address = address;
         this.contact = contact;
         this.statuses = statuses;
+    }
+
+    public boolean getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public String[] getStatuses() {
+        return statuses;
     }
 
     @Override
