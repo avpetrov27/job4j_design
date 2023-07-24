6e32465401963518a68f50e82d4c3e073190c5fe
avpetrov27 <avpetrov27@yandex.ru>
2023-07-24 16:23:42 +0300

    0. Что такое база данных. [#6860]

---
+++ db/20230724_001_What_is_a_database.sql
@@ -0,0 +1,18 @@
+create table Customers(
+      id serial primary key,
+      name varchar(255),
+	  age numeric(3,0),
+      gender varchar(1)
+);
+
+insert into Customers (name, age, gender) values ('Petrov', 34, 'М');
+
+select * from Customers;
+
+update Customers set name = 'Petrov A V';
+
+select * from Customers;
+
+delete from Customers;
+
+select * from Customers;
