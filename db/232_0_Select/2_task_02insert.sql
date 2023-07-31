f4edf90b19d71a733abe7edb5d9c5fe60da084e2
avpetrov27 <avpetrov27@yandex.ru>
2023-07-31 14:18:09 +0300

    0. Select [#6861]

---
+++ db/232_0_Select/2_task_02insert.sql
@@ -0,0 +1,6 @@
+insert into fauna (name, avg_age, discovery_date) values ('AB', 1, '10.09.2020');
+insert into fauna (name, avg_age, discovery_date) values ('ZZ', 50, '31.07.2023');
+insert into fauna (name, avg_age, discovery_date) values ('QQ', 100, '01.01.0001');
+insert into fauna (name, avg_age, discovery_date) values ('fish', 100, '01.01.1999');
+insert into fauna (name, avg_age, discovery_date) values ('superfish', 15000, '01.01.1999');
+insert into fauna (name, avg_age) values ('mysteriousfish', 30000);
